9841ae3f5e57d8f0cc2996352dd59fc342764622
Aeon <vodkaa@vodkaa.dev>
2025-03-24 21:08:08 -0600

    added songcol, maybe modified changewall

--- computerbackup.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-rsync -r -t --delete ~/Pictures/wallpapers monarch@10.0.0.247:/store/pub/media/
-rsync -r -t -p --delete ~/.config/ monarch@10.0.0.247:/store/pub/vault/backups/archconfig
-rsync -r -t --delete ~/Documents/ monarch@10.0.0.247:/store/vault/backups/Documents
